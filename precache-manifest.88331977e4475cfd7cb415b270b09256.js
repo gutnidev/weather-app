615619633035c8b7c09a3185a5b45784cfb3a6ef
gutnivlad <gutnivlad@gmail.com>
2020-08-21 15:07:05 +0300

    deploy

---
+++ precache-manifest.88331977e4475cfd7cb415b270b09256.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9a340064c779817fa1",
+    "url": "/weather-app/css/app.e4387c96.css"
+  },
+  {
+    "revision": "728f9b4d00b8c8443987639ed2fbbe1e",
+    "url": "/weather-app/img/loader.728f9b4d.gif"
+  },
+  {
+    "revision": "604033306f9a7c70f513b818135e1f52",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "8a9a340064c779817fa1",
+    "url": "/weather-app/js/app.1a108238.js"
+  },
+  {
+    "revision": "91c2277fa3fbfc1d8052",
+    "url": "/weather-app/js/chunk-vendors.38ba86c1.js"
+  },
+  {
+    "revision": "8a6279477f72390c93140636765dd049",
+    "url": "/weather-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/weather-app/robots.txt"
+  }
+]);
